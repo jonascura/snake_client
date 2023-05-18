a1e31a90f398b6241386d484d553816d6ed1059a
Jonas Cura <jonasm.cura@gmail.com.com>
2023-05-17 19:57:36 -0600

    Add client

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+// establishes connection with game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  // interperate the data as text
+  conn.setEncoding("utf8");
+  
+  conn.on("connect", () => {
+    console.log("Server kicked you out due to inactivity.")
+  });
+
+  return conn;
+  
+};
+
+console.log("Connecting ...");
+
+connect();
